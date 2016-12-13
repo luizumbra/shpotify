0d675be3ce231c2eea4b02d448bd47a8b2978b2d
Luiz Antonio Marques Ferreira <luiz_fantasma@hotmail.com>
2016-12-13 14:32:01 -0200

    Create alarm-clock.sh
    Alarm clock algorithm

---
+++ alarm-clock.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Define alarm time
+
+# Define snooze for up volume
+
+# Initialize sptify with volum at 20%
+
+# Start alarm on time
+
+# Incrise the volum each snooze time by 5%
+
+# Interrupt alarm by event key
